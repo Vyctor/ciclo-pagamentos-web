849bccda5b7e4dc439b55331ef75064926c966c9
Vyctor <dev.vyctor@gmail.com>
2021-12-13 22:29:29 -0300

    ajustando chamada a api de ciclo de pagamentos

--- app/dashboard/dashboardController.js
+++ app/dashboard/dashboardController.js
@@ -10,13 +10,14 @@
       $http
         .get(url)
         .then((response) => {
-          vm.credit = response.credit;
-          vm.debit = response.debit;
-          vm.total = vm.credit - vm.debit;
+          console.log(response);
+          vm.credit = response.data.credits;
+          vm.debt = response.data.debts;
+          vm.total = response.data.total;
         })
         .catch((error) => {
           vm.credit = 0;
-          vm.debit = 0;
+          vm.debt = 0;
           vm.total = 0;
         });
     };
