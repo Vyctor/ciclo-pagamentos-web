6740a16e2d2fe92ca75acdfb1507ca2a9bcb5abe
Vyctor <dev.vyctor@gmail.com>
2021-12-14 22:10:22 -0300

    alteracao do ciclo de pagamentos

--- app/billingCycle/billingCycleController.js
+++ app/billingCycle/billingCycleController.js
@@ -48,8 +48,22 @@
       tabsFactory.show(vm, { tabDelete: true });
     };
 
+    vm.update = function () {
+      const updateUrl = `${url}/${vm.billingCycle.id}`;
+
+      $http
+        .put(updateUrl, vm.billingCycle)
+        .then((response) => {
+          vm.refresh();
+          messageFactory.addSuccess("Operação realizada com sucesso!");
+        })
+        .catch((error) => {
+          messageFactory.addError("Tente novamente em alguns instantes.");
+        });
+    };
+
     vm.delete = function () {
-      const deleteUrl = `${vm.url}/${vm.billingCycle.id}`;
+      const deleteUrl = `${url}/${vm.billingCycle.id}`;
 
       $http
         .delete(url, vm.billingCycle)
